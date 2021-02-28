0bc981c9f315dc24107980e332aa306e75cd9578
Fizzika <Fizzika.NET@yandex.ru>
2021-02-28 18:15:56 +0300

    Turret/Hull/Mode analyze

--- service.py
+++ service.py
@@ -1,4 +1,5 @@
 import os
+
 import db
 import api
 
@@ -9,18 +10,39 @@ TURRET_COUNT = 15
 
 MODULES_MAP = db.json.loads("{\"Fox\":\"Firebird\",\"Badger\":\"Freeze\",\"Ocelot\":\"Isida\",\"Wolf\":\"Hammer\",\"Panther\":\"Twins\",\"Lion\":\"Ricochet\",\"Dolphin\":\"Smoky\",\"Orka\":\"Striker\",\"Shark\":\"Vulcan\",\"Grizzly\":\"Thunder\",\"Falcon\":\"Railgun\",\"Griffin\":\"Magnum\",\"Owl\":\"Gauss\",\"Eagle\":\"Shaft\",\"Spider\":\"Mines\"}")
 
-def calculate_module_stat():
+def get_played_logins():
     users = load_users()
     logins = []
     for user in users:
         if ('state' in user) and (user['state']['rank'] > 30):
             logins.append(user['login'])
+    return logins
+
+def get_usage_stat(role, month = 'currMonth'):
+    logins = get_played_logins()
+    res = {}
+    total = len(logins)
+    for login in logins:
+        user = load_user(login)
+        activities = user[month]['activities'] if month in user else []
+        target_group = list(filter(lambda act: act['role'] == role, activities))
+        if role == 'Module' and len(list(filter(lambda mod: mod['name'].find('Spectrum') != -1, target_group))) != 0:
+            continue
+        if (len(target_group) == 0):
+            total = total - 1
+        for entity in target_group:
+            name = entity['name']
+            res[name] = res.get(name, 0) + entity['time']
     
+    print(total)
+    key_mapper = lambda k: MODULES_MAP[k] if role == 'Module' else k
+    return dict((key_mapper(k), v) for k, v in sorted(res.items(), key=lambda kv: kv[1]))
+
+def calculate_module_stat():
+    logins = get_played_logins()
     stat = list([0.0 for _ in range(0, TURRET_COUNT)])
     ideal_stat = list([0.0 for _ in range(0, TURRET_COUNT)])
     stat_count = 0
-
-    modules_stat = {}
 
     for login in logins:
         user = load_user(login)
@@ -28,11 +50,6 @@ def calculate_module_stat():
         modules = list(filter(lambda act: act['role'] == 'Module', activities))
         if len(list(filter(lambda mod: mod['name'].find('Spectrum') != -1, modules))) != 0:
             continue
-        # Update modules_stat
-        for module in modules:
-            name = module['name']
-            modules_stat[name] = modules_stat.get(name, 0) + module['time']
-
         times = sorted(map(lambda x: x['time'], modules), reverse=True)
         ideal_times = [1.0/TURRET_COUNT for _ in range(0, TURRET_COUNT)]
         full_time = sum(times)
@@ -45,32 +62,31 @@ def calculate_module_stat():
 
     stat = list(map(lambda x: x / stat_count, stat))
     ideal_stat = list(map(lambda x: x / stat_count, ideal_stat))
-
-    modules_stat = dict((MODULES_MAP[name], val) for name, val in sorted(modules_stat.items(), key=lambda kv: kv[1]))
     
+    return (stat, ideal_stat)
 
-    print("Total players")
-    print(stat_count)
-    print("Percent stat")
-    print(stat)
-    print("Modules stat")
-    print(modules_stat)
-
-    # Uncomment to plot graph
-
-    # plot_module_stat(modules_stat)
-    # plot_module_pie(modules_stat)
-    plot_stat(stat)
-    # plot_both_stat(stat, ideal_stat)
-
-def plot_module_stat(module_stat: dict):
+def plot_all_usages_pie(usage: dict):
     plt.rcdefaults()
-    plt.barh(list(module_stat.keys()), list(map(lambda h: h/3600, module_stat.values())))
+    for name in ['Module', 'Hull', 'Turret', 'Mode']:    
+        plt.figure(name)
+        plt.title(name + ' usage', fontdict={'fontsize': 'large', 'fontweight': 'bold'})
+        plot_on_axe(plt, usage[name])
+
     plt.show()
 
-def plot_module_pie(module_stat: dict):
+
+def plot_on_axe(plt, usage_stat: dict):
+
+    def get_labels():
+        total = sum(usage_stat.values())
+        return [f'{name} [{round(time/total * 100)}%]' for name, time in usage_stat.items()]
+
+    plt.pie(list(usage_stat.values()), labels=get_labels())
+
+
+def plot_usage_pie(usage_stat: dict):
     plt.rcdefaults()
-    plt.pie(list(module_stat.values()), labels=list(module_stat.keys()))
+    plt.pie(list(usage_stat.values()), labels=list(usage_stat.keys()))
     plt.show()
 
 def plot_stat(stat: list):
